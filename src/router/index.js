5f1ccca89f2285e4f9d4dd3103c2ca7017e4555a
unknown <negulyaev2017@gmail.com>
2024-04-29 03:04:39 +0300

    feature/TT-57: add login, podcasts, podcast routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,29 +5,29 @@ import PodcastView from '../views/PodcastView.vue'
 import HistoryView from '../views/HistoryView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/login',
-    //   name: 'Login',
-    //   component: LoginView
-    // },
-    {
-      path: '/podcasts',
-      name: 'Podcasts',
-      component: PodcastsView
-    },
-    {
-      path: '/podcasts/:id',
-      name: 'Podcast',
-      component: PodcastView
-    },
-    // {
-    //   path: '/history',
-    //   name: 'History',
-    //   component: HistoryView
-    // }
-  ]
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
+    routes: [{
+            path: '/login',
+            name: 'Login',
+            component: LoginView
+        },
+        {
+            path: '/podcasts',
+            name: 'Podcasts',
+            component: PodcastsView
+        },
+        {
+            path: '/podcasts/:id',
+            name: 'Podcast',
+            component: PodcastView
+        },
+        // {
+        //   path: '/history',
+        //   name: 'History',
+        //   component: HistoryView
+        // }
+    ]
 })
 
 export default router
