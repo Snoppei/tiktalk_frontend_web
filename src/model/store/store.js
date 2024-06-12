e8caa9bc406b125f134f619bf44d5d3c7d03aa72
Snoppei <44434924+Snoppei@users.noreply.github.com>
2024-06-12 16:15:20 +0300

    feature/TT-97: add metadata loading from minio

--- src/model/store/store.js
+++ src/model/store/store.js
@@ -1,154 +1,182 @@
 import { 
-    getPodcasts, 
-    getPodcastById, 
-    getPersonById, 
-    getReportsByPodcastId, 
-    banPodcast, 
-    rejectReports, 
-    getHistoryPodcasts, 
-    getHistoryPodcastById 
-  } from '../api'; 
-  
-  export const podcastsStore = {
-    state: {
-      podcasts: [],
-      history: [],
-      currentPage: 1, 
-      pageSize: 10,
-      sortParam: 'REPORTS_COUNT_DESC' 
+  getPodcasts, 
+  getPodcastById, 
+  getPersonById, 
+  getReportsByPodcastId, 
+  banPodcast, 
+  rejectReports, 
+  getHistoryPodcasts, 
+  getHistoryPodcastById 
+} from '../api'; 
+
+import { createStore } from 'vuex';
+import { getAudioDuration } from '../minioapi'; // Путь к вашему minioapi.js
+
+
+
+export const podcasts = {
+  namespaced: true,
+  state: {
+    podcasts: [],
+    history: [],
+    currentPage: 1, 
+    pageSize: 15,
+    sortParam: 'REPORTS_COUNT_DESC' 
+  },
+  mutations: {
+    SET_PODCASTS(state, podcasts) {
+      state.podcasts = podcasts;
     },
-    mutations: {
-      SET_PODCASTS(state, podcasts) {
-        state.podcasts = podcasts;
-      },
-      SET_HISTORY(state, history) {
-        state.history = history;
-      },
-      DELETE_PODCAST(state, payload) {
-        const { podcastId, solution } = payload;
-        const index = state.podcasts.findIndex(podcast => podcast.id === podcastId);
-        if (index !== -1) {
-          state.history.push({
-            ...state.podcasts[index],
-            decision: "Подкаст удален",
-            solution: solution
-          });
-          state.podcasts.splice(index, 1);
-        }
-      },
-      REJECT_COMPLAINTS(state, payload) {
-        const { podcastId, solution } = payload;
-        const index = state.podcasts.findIndex(podcast => podcast.id === podcastId);
-        if (index !== -1) {
-          state.history.push({
-            ...state.podcasts[index],
-            decision: "Жалобы отклонены",
-            solution: solution
-          });
-          state.podcasts.splice(index, 1);
-        }
-      },
-      SET_CURRENT_PAGE(state, page) {
-        state.currentPage = page;
-      },
-      SET_PAGE_SIZE(state, size) { 
-        state.pageSize = size;
-      },
-      SET_SORT_PARAM(state, sortParam) {
-        state.sortParam = sortParam;
-      },
+    SET_HISTORY(state, history) {
+      state.history = history;
     },
-    actions: {
-      async fetchPodcasts({ commit, state }) {
-        try {
-          const response = await getPodcasts(
-            state.currentPage - 1, 
-            state.pageSize, 
-            state.sortParam
-          );
-          commit('SET_PODCASTS', response.data);
-        } catch (error) {
-          console.error('Ошибка при загрузке подкастов:', error);
-        }
-      },
-      async fetchPodcastDetails({ commit }, podcastId) {
-        try {
-          const response = await getPodcastById(podcastId);
-          return response.data;
-        } catch (error) {
-          console.error('Ошибка при загрузке подкаста:', error);
-          throw error; 
-        }
-      },
-      async fetchAuthor({ commit }, personId) {
-        try {
-          const response = await getPersonById(personId);
-          return response.data;
-        } catch (error) {
-          console.error('Ошибка при загрузке автора:', error);
-          throw error;
-        }
-      },
-      async fetchReports({ commit }, { podcastId, page, size, sortParam }) {
-        try {
-          const response = await getReportsByPodcastId(podcastId, page, size, sortParam);
-          return response.data;
-        } catch (error) {
-          console.error('Ошибка при загрузке жалоб:', error);
-          throw error;
-        }
-      },
-      async deletePodcast({ commit }, { podcastId, solution }) {
-        try {
-          await banPodcast(podcastId, solution);
-          commit('DELETE_PODCAST', { podcastId, solution });
-        } catch (error) {
-          console.error('Ошибка при удалении подкаста:', error);
-          throw error; 
-        }
-      },
-      async rejectComplaints({ commit }, { podcastId, solution }) {
-        try {
-          await rejectReports(podcastId, solution);
-          commit('REJECT_COMPLAINTS', { podcastId, solution });
-        } catch (error) {
-          console.error('Ошибка при отклонении жалоб:', error);
-          throw error; 
-        }
-      },
-      async fetchHistoryPodcasts({ commit, state }) {
-        try {
-          const response = await getHistoryPodcasts(state.currentPage, state.pageSize, state.sortParam);
-          commit('SET_HISTORY', response.data);
-        } catch (error) {
-          console.error('Ошибка при загрузке истории подкастов:', error);
-        }
-      },
-      async fetchHistoryPodcastDetails({ commit }, podcastId) {
-        try {
-          const response = await getHistoryPodcastById(podcastId);
-          return response.data;
-        } catch (error) {
-          console.error('Ошибка при загрузке подкаста из истории:', error);
-          throw error; 
-        }
-      },
-      setCurrentPage({ commit }, page) {
-        commit('SET_CURRENT_PAGE', page);
-      },
-      setPageSize({ commit }, size) {
-        commit('SET_PAGE_SIZE', size);
-      },
-      setSortParam({ commit }, sortParam) {
-        commit('SET_SORT_PARAM', sortParam);
-      },
+    DELETE_PODCAST(state, payload) {
+      const { podcastId, solution } = payload;
+      const index = state.podcasts.findIndex(podcast => podcast.id === podcastId);
+      if (index !== -1) {
+        state.history.push({
+          ...state.podcasts[index],
+          decision: "Подкаст удален",
+          solution: solution
+        });
+        state.podcasts.splice(index, 1);
+      }
     },
-    getters: {
-      getPodcastById: (state) => (id) => {
-        return state.podcasts.find(podcast => podcast.id === id);
-      },
-      getHistoryPodcastById: (state) => (id) => {
-        return state.history.find(podcast => podcast.id === id);
-      },
+    REJECT_COMPLAINTS(state, payload) {
+      const { podcastId, solution } = payload;
+      const index = state.podcasts.findIndex(podcast => podcast.id === podcastId);
+      if (index !== -1) {
+        state.history.push({
+          ...state.podcasts[index],
+          decision: "Жалобы отклонены",
+          solution: solution
+        });
+        state.podcasts.splice(index, 1);
+      }
     },
-  };
+    SET_CURRENT_PAGE(state, page) {
+      state.currentPage = page;
+    },
+    SET_PAGE_SIZE(state, size) { 
+      state.pageSize = size;
+    },
+    SET_SORT_PARAM(state, sortParam) {
+      state.sortParam = sortParam;
+    },
+    SET_PODCAST_DURATION(state, { podcastId, duration }) {
+      const index = state.podcasts.findIndex(podcast => podcast.audioUrl === podcastId); // Используем audioUrl как id
+      if (index !== -1) {
+        state.podcasts[index].duration = duration;
+      }
+    },
+  },
+  actions: {
+    async fetchPodcasts({ commit, state, dispatch }) {
+      try {
+        const response = await getPodcasts(
+          state.currentPage - 1,
+          state.pageSize,
+          state.sortParam
+        );
+        commit('SET_PODCASTS', response.data);
+      } catch (error) {
+        console.error('Ошибка при загрузке подкастов:', error);
+      }
+    },
+    setCurrentPage({ commit, dispatch }, page) {
+      commit('SET_CURRENT_PAGE', page);
+      localStorage.setItem('currentPage', page);
+      dispatch('fetchPodcasts');
+    },
+    async fetchPodcastDetails({ commit }, podcastId) {
+      try {
+        const response = await getPodcastById(podcastId);
+        return response.data;
+      } catch (error) {
+        console.error('Ошибка при загрузке подкаста:', error);
+        throw error; 
+      }
+    },
+    async fetchAuthor({ commit }, personId) {
+      try {
+        const response = await getPersonById(personId);
+        return response.data;
+      } catch (error) {
+        console.error('Ошибка при загрузке автора:', error);
+        throw error;
+      }
+    },
+    async fetchReports({ commit }, { podcastId, page, size, sortParam }) {
+      try {
+        const response = await getReportsByPodcastId(podcastId, page, size, sortParam);
+        return response.data;
+      } catch (error) {
+        console.error('Ошибка при загрузке жалоб:', error);
+        throw error;
+      }
+    },
+    async deletePodcast({ commit }, { podcastId, solution }) {
+      try {
+        await banPodcast(podcastId, solution);
+        commit('DELETE_PODCAST', { podcastId, solution });
+      } catch (error) {
+        console.error('Ошибка при удалении подкаста:', error);
+        throw error; 
+      }
+    },
+    async rejectComplaints({ commit }, { podcastId, solution }) {
+      try {
+        await rejectReports(podcastId, solution);
+        commit('REJECT_COMPLAINTS', { podcastId, solution });
+      } catch (error) {
+        console.error('Ошибка при отклонении жалоб:', error);
+        throw error; 
+      }
+    },
+    async fetchHistoryPodcasts({ commit, state }) {
+      try {
+        const response = await getHistoryPodcasts(state.currentPage, state.pageSize, state.sortParam);
+        commit('SET_HISTORY', response.data);
+      } catch (error) {
+        console.error('Ошибка при загрузке истории подкастов:', error);
+      }
+    },
+    async fetchHistoryPodcastDetails({ commit }, podcastId) {
+      try {
+        const response = await getHistoryPodcastById(podcastId);
+        return response.data;
+      } catch (error) {
+        console.error('Ошибка при загрузке подкаста из истории:', error);
+        throw error; 
+      }
+    },
+    setPageSize({ commit }, size) {
+      commit('SET_PAGE_SIZE', size);
+    },
+    setSortParam({ commit }, sortParam) {
+      commit('SET_SORT_PARAM', sortParam);
+    },
+    async fetchPodcastDuration({ commit }, audioUrl) {
+      try {
+        const duration = await getAudioDuration(audioUrl);
+        commit('SET_PODCAST_DURATION', { podcastId: audioUrl, duration }); 
+      } catch (error) {
+        console.error('Ошибка при получении длительности:', error);
+        // Обработка ошибки:
+        commit('SET_PODCAST_DURATION', { 
+          podcastId: audioUrl, 
+          duration: null, 
+          durationError: 'Не удалось получить длительность' 
+        });
+      }
+    }
+  },
+  getters: {
+    getPodcastById: (state) => (id) => {
+      return state.podcasts.find(podcast => podcast.id === id);
+    },
+    getHistoryPodcastById: (state) => (id) => {
+      return state.history.find(podcast => podcast.id === id);
+    },
+  },
+};
