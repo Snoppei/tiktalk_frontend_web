e53d78436e1b6fd5ab3fd05d84eb34614720a5c8
Snoppei <44434924+Snoppei@users.noreply.github.com>
2024-06-12 16:16:31 +0300

    feature/TT-97: minor 2

--- src/model/minioapi.js
+++ src/model/minioapi.js
@@ -16,45 +16,9 @@ export async function getAudioDuration(audioUrl) {
     const response = await axios.get(audioUrl, { responseType: 'blob' });
     const metadata = await parseBlob(response.data);
 
-    // Возвращаем null, если длительность не найдена в метаданных
     return metadata.format.duration || null; 
   } catch (error) {
     console.error('Ошибка получения длительности:', error);
-    return null; // Возвращаем null в случае ошибки
+    return null;
   }
 }
-
-// async function getDurationFromMetadata(audioUrl) {
-//   return new Promise((resolve, reject) => {
-//     jsmediatags.read(audioUrl, {
-//       onSuccess: function(tag) {
-//         const duration = tag.tags.duration; // Длительность в секундах (может быть undefined)
-//         resolve(duration);
-//       },
-//       onError: function(error) {
-//         console.error('Ошибка чтения метаданных:', error);
-//         reject(error);
-//       },
-//     });
-//   });
-// }
-
-// export async function getAudioDuration(audioUrl) {
-//   try {
-//     console.log('Запрос к Minio:', audioUrl);
-
-//     const response = await minioClient.get(audioUrl, { responseType: 'blob' });
-//     console.log('Ответ Minio:', response);
-
-//     const blob = response.data;
-//     console.log('Blob:', blob);
-
-//     const duration = await new MP3Duration(blob).getDuration(); // Создаем объект MP3Duration и вызываем метод getDuration()
-//     console.log('Длительность:', duration); // Проверка
-//     return duration;
-
-//   } catch (error) {
-//     console.error('Ошибка при получении длительности:', error);
-//     throw error;
-//   }
-// }
